d04ca7a38711922319f0cbe588809b5ba41e22ac
Gilgames <balintkovacs11@gmail.com>
2024-01-05 21:01:42 +0100

    fix misunderstood syntax and match the maximum number of choices

--- cogs/randomfoxcog.py
+++ cogs/randomfoxcog.py
@@ -29,44 +29,36 @@ class RandomFoxCog(commands.Cog):
     @app_commands.command(name="animal", description="Random image of a given animal")
     @app_commands.choices(
         animal=[
-            app_commands.Choice(name="African Wild dog", value="chi"),
             app_commands.Choice(name="Bear", value="bear"),
-            app_commands.Choice(name="Dog", value="dog"),
             app_commands.Choice(name="Capybara", value="capy"),
             app_commands.Choice(name="Caracal", value="caracal"),
             app_commands.Choice(name="Cheetah", value="chee"),
             app_commands.Choice(name="Coyote", value="yote"),
             app_commands.Choice(name="Deer", value="bleat"),
-            app_commands.Choice(name="Ferret", value="dook"),
             app_commands.Choice(name="Fox", value="fox"),
             app_commands.Choice(name="Hyena", value="yeen"),
             app_commands.Choice(name="Jaguar", value="jaguar"),
-            app_commands.Choice(name="Lion", value="leo"),
             app_commands.Choice(name="Lynx", value="lynx"),
             app_commands.Choice(name="Maned Wolf", value="mane"),
             app_commands.Choice(name="Puma", value="puma"),
-            app_commands.Choice(name="Opossum", value="poss"),
             app_commands.Choice(name="Otter", value="ott"),
             app_commands.Choice(name="Manul", value="manul"),
-            app_commands.Choice(name="Pine Marten", value="marten"),
             app_commands.Choice(name="Bunny", value="bun"),
             app_commands.Choice(name="Raccoon", value="racc"),
             app_commands.Choice(name="Red Panda", value="wah"),
             app_commands.Choice(name="Serval", value="serval"),
-            app_commands.Choice(name="Shiba Inu", value="shiba"),
-            app_commands.Choice(name="Skunk", value="skunk"),
             app_commands.Choice(name="Snake", value="snek"),
             app_commands.Choice(name="Snow Leopard", value="snep"),
             app_commands.Choice(name="Tiger", value="tig"),
             app_commands.Choice(name="Wolf", value="woof"),
         ]
     )
-    async def random_animals(self, interaction: discord.Interaction, animal: str):
+    async def random_animals(self, interaction: discord.Interaction, animal: app_commands.Choice[str]):
         await interaction.response.defer()
         try:
-            img = requests.get(f"https://api.tinyfox.dev/img?animal={animal}").content
+            img = requests.get(f"https://api.tinyfox.dev/img?animal={animal.value}").content
             await interaction.followup.send(
-                file=discord.File(io.BytesIO(img), f"{animal}.png")
+                file=discord.File(io.BytesIO(img), f"{animal.value}.png")
             )
         except Exception as e:
             await interaction.followup.send(str(e))
