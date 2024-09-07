ddb381f74948edecca95f41f73c14f66e39cb0d0
Kovács Bálint <balintkovacs11@gmail.com>
2024-09-08 00:53:00 +0200

    first pass at implementing neptunfej

--- cogs/captioncog.py
+++ cogs/captioncog.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 from util.msgutil import devcheck, errorembed, errorrespond
 
 from caption.src.pipeline import caption
+import neptunfej
 
 class CaptionCog(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
@@ -29,6 +30,21 @@ class CaptionCog(commands.Cog):
         await interaction.response.defer()
         try:
             out = caption(link, text, force_gif, gif_transparency)
+            await interaction.followup.send(text, file=discord.File(out))
+        except Exception as e:
+            await interaction.followup.send(embed=errorembed(str(e)))
+
+    # neptunfej
+    @app_commands.allowed_installs(guilds=True, users=True)
+    @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+    @app_commands.command(name="neptun", description="/pa")
+    async def neptun(self, interaction: discord.Interaction, text: str = "forgot the caption") -> None:
+        if not await devcheck(interaction):
+            return
+
+        await interaction.response.defer()
+        try:
+            out = neptunfej.generate(text, 2, (10, 10))
             await interaction.followup.send(text, file=discord.File(out))
         except Exception as e:
             await interaction.followup.send(embed=errorembed(str(e)))
